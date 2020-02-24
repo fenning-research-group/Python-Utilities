ae194061d02f4729f27b14d4960c85c9a42dafa2
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-02-24 11:43:22 -0800

    add dependencies to setup.py

--- FrgTools/setup.py
+++ FrgTools/setup.py
@@ -4,6 +4,10 @@ setup(
     name='frgtools',
     version='0.1dev',
     packages=['frgtools'],
+    install_requires = [
+    	'matplotlib-scalebar',
+    	'imreg_dft'
+    	],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('./frgtools/README.txt').read(),
 )
