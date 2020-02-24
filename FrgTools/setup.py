980e6f4d82ab90f3d76f1d7d058c50554d89ccb2
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-02-24 11:46:36 -0800

    update author info + license

--- FrgTools/setup.py
+++ FrgTools/setup.py
@@ -4,10 +4,12 @@ setup(
     name='frgtools',
     version='0.1dev',
     packages=['frgtools'],
+    author = 'Rishi Kumar',
+    author_email = 'rishikumar@ucsd.edu',
     install_requires = [
     	'matplotlib-scalebar',
     	'imreg_dft'
     	],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='MIT',
     long_description=open('./frgtools/README.txt').read(),
 )
