98fe0e93ecf968165b9826d1f70734b2ac3d1083
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-03-10 09:47:57 -0700

    frg listdir now returns absolute filepaths

--- FrgTools/frgtools/misc.py
+++ FrgTools/frgtools/misc.py
@@ -2,7 +2,7 @@ import os
 
 def listdir(path, display = True):
 	exclude = ['desktop.ini']
-	fids = [os.path.join(path, x) for x in os.listdir(path) if not any([exclude_ in x for exclude_ in exclude])]
+	fids = [os.path.abspath(os.path.join(path, x)) for x in os.listdir(path) if not any([exclude_ in x for exclude_ in exclude])]
 	if display:
 		print('Files in \'{}\':'.format(path))
 		for i, f in enumerate(fids):
