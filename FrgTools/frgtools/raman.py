5262ffe5dea78df6b1361a545b0c3df6be613006
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-11-15 17:39:23 -0800

    add tauc code

--- FrgTools/frgtools/raman.py
+++ FrgTools/frgtools/raman.py
@@ -3,11 +3,48 @@ import numpy as np
 from tqdm import tqdm
 
 def shifttonm(shift, incident_wl):
+    '''
+    converts a raman shift (cm-1) to wavelength (nm)
+
+    inputs
+        shift: raman shift, cm-1
+        incident_wl: excitation source wavelength, nm
+
+    output
+        emitted wavelength, nm
+    '''
     incident_wn = 1/(incident_wl * 1e-7)
     exit_wn = incident_wn - shift
     return 1e7/exit_wn 
 
+def nmtoshift(wl, incident_wl):
+    '''
+    converts an emitted wavelength (nm) to raman shift (cm-1)
+
+    inputs
+        wl: emitted wavelength, nm
+        incident_wl: excitation source wavelength, nm
+
+    output
+        shift: raman shift, cm-1
+    '''
+    incident_wn = 1/(incident_wl * 1e-7)
+    exit_wn = 1/(wl * 1e-7)
+    return incident_wn - exit_wn
+
 def load_renishaw(path, incident_wl):
+    '''
+    Loads raw spectra files output when exporting renishaw area scans to xy data.
+    Assumes input data is in raman shift (cm-1)
+
+    inputs:
+        path: path to folder holding all raw xy files
+        incident_wl: excitation source wavelength, nm
+
+    output:
+        dictionary with consolidated area scan data
+
+    '''
     fids = [os.path.join(path, x) for x in os.listdir(path)]
     allx = []
     ally = []
@@ -23,7 +60,7 @@ def load_renishaw(path, incident_wl):
     
     temp = np.loadtxt(fids[0])
     shift = temp[:,0]
-    wl = ShiftToNm(shift, incident_wl = incident_wl)
+    wl = shifttonm(shift, incident_wl = incident_wl)
     
     cts = np.zeros((len(y), len(x), len(wl)))
     for f, x_, y_ in tqdm(zip(fids, allx, ally), total = len(fids), desc = 'Loading Renishaw Scans', leave = False):
--- FrgTools/frgtools/uvvis.py
+++ FrgTools/frgtools/uvvis.py
@@ -1,6 +1,9 @@
 import os
 import csv
 import numpy as np
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
+
 
 def load_lambda(fpath):
 	def readLambdaCSV(fpath):
@@ -50,3 +53,58 @@ def load_lambda(fpath):
 			}
 			
 	return data
+
+def tauc(wl, a, bandgap_type, fit_width = None, fit_threshold = 0.1, plot = False):
+	wl = np.array(wl)
+	a = np.array(a)
+
+	if fit_width is None:
+		fit_width = len(wl)//20  #default to 5% of data width
+
+	fit_pad = fit_width//2
+
+	if str.lower(bandgap_type) == 'direct':
+		n = 0.5
+	elif str.lower(bandgap_type) == 'indirect':
+		n = 2
+	else:
+		raise ValueError('argument "bandgap_type" must be provided as either "direct" or "indirect"')
+
+
+	c = 3e8                    	#speed of light, m/s
+	h = 4.13567e-15            	#planck's constant, eV
+	nu = c/(wl*1e-9)  			#convert nm to hz
+	ev = 1240/wl     			#convert nm to ev
+		  
+	taucvalue = (a*h*nu) ** (1/n)
+	taucvalue_threshold = taucvalue.max() * fit_threshold
+	best_slope = None
+	best_intercept = None
+	best_r2 = 0
+
+	for idx in range(fit_pad, len(wl) - fit_pad):
+		if taucvalue[idx] >= taucvalue_threshold:
+			fit_window = slice(idx-fit_pad, idx+fit_pad)
+			slope, intercept, rval, _, _ = linregress(ev[fit_window], taucvalue[fit_window])
+			r2 = rval**2
+			if r2 > best_r2 and slope > 0:
+				best_r2 = r2
+				best_slope = slope
+				best_intercept = intercept
+
+	Eg = -best_intercept / best_slope #x intercept
+
+	if plot:
+		fig, ax = plt.subplots()
+		ax.plot(ev, taucvalue, 'k')
+		ylim0 = ax.get_ylim()
+		ax.plot(ev, ev*best_slope + best_intercept, color = plt.cm.tab10(3), linestyle = ':')
+		ax.set_ylim(*ylim0)
+		ax.set_xlabel('Photon Energy (eV)')
+		if n == 0.5:
+			ax.set_ylabel(r'$({\alpha}h{\nu})^2$');   
+		else:
+			ax.set_ylabel(r'$({\alpha}h{\nu})^{1/2}$');
+		plt.show()
+
+	return Eg
